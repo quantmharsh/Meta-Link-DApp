a6e7040a90f258a5b3d9eed0da836838fc95af28
Harsh Srivastava <srivastavah240@gmail.com>
2025-02-17 17:30:41 +0530

    (feat):created basic structure for User Display

--- app/onboard/components/user-profile-display.tsx
+++ app/onboard/components/user-profile-display.tsx
@@ -1,6 +1,16 @@
-
-import React from 'react'
-
+import React from "react";
+import {
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandTiktok,
+  IconBrandX,
+  IconBrandYoutube,
+  IconBriefcase,
+  IconMail,
+  IconMapPin,
+  IconPhone,
+} from "@tabler/icons-react";
+import Link from "next/link";
 interface UserProfileDisplayProps {
   formData:any ;
   countryCode:string ;
@@ -8,7 +18,14 @@ interface UserProfileDisplayProps {
 
 const UserProfileDisplay:React.FC<UserProfileDisplayProps> =({formData  , countryCode}) => (
  
-    <div>user-profile-display</div>
+    <div className="  group relative border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px]  hover:border-purple-500 hover:border-[4px]
+     "> 
+  <div className="h-[32px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute  -left-[17px] top-[74px] rounded-s-lg group-hover:-left-[7px] group-hover:top-[72px]"></div>
+    <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[124px] rounded-s-lg group-hover:-left-[7px]"></div>
+    <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[178px] rounded-s-lg group-hover:-left-[7px]"></div>
+
+     
+      </div>
   
 )
 
